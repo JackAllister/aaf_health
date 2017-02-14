b0d3b9a49afed67c04b36764e5c495eef5c3a30f
JackAllister <b3042098@my.shu.ac.uk>
2017-02-14 17:02:13 +0000

    Added in checks for existing user & invalid email

--- api/controllers/authentication.js
+++ api/controllers/authentication.js
@@ -1,5 +1,6 @@
 var passport = require('passport');
 var mongoose = require('mongoose');
+var validator = require('validator');
 var User = mongoose.model('User');
 
 module.exports.register = function(req, res) {
@@ -18,21 +19,45 @@ module.exports.register = function(req, res) {
     return;
   }
 
-  var user = new User();
-
-  user.name = req.body.name;
-  user.email = req.body.email;
-
-  user.setPassword(req.body.password);
-
-  user.save(function(err) {
-    var token = user.generateJwt();
-
+  /* Check that a valid email exists */
+  if (!validator.isEmail(req.body.email)) {
     res.status(200);
     res.json({
-      "token": token
+      "message": "Please use a valid email address"
     });
-  });
+    return;
+  }
+
+
+  /* Check to see if an email address already exists */
+  User.findOne({"email": req.body.email})
+    .exec(function(err, user) {
+      if (err === null) {
+        if (user === null) {
+          /* If no user found with email create new user */
+          var user = new User();
+          user.name = req.body.name;
+          user.email = req.body.email;
+
+          user.setPassword(req.body.password);
+
+          user.save(function(err) {
+            var token = user.generateJwt();
+            res.status(200);
+            res.json({
+              "token": token
+            });
+          });
+
+        } else {
+          res.status(200);
+          res.json({"message": "An account with that email already exists."});
+        }
+      } else {
+        res.status(500);
+        res.json({"message": "Error searching in database."});
+      }
+    });
 };
 
 module.exports.login = function(req, res) {
