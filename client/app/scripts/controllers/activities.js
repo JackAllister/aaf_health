6831d0f25fa1b197ba0fe5e020cc3484cd5c0c0e
JackAllister <b3042098@my.shu.ac.uk>
2017-02-15 15:44:00 +0000

    Refactored activities controller in client

--- client/app/scripts/controllers/activities.js
+++ client/app/scripts/controllers/activities.js
@@ -18,6 +18,27 @@ angular.module('clientApp')
     this.message = '';
     this.myActivities = [];
 
+    function getNameFromPostedBy(activity, index) {
+
+      /* We need to make individual request to get name */
+      profileService.getUserDetails(activity.postedBy,
+        function(nameResult, response) {
+          if (nameResult) {
+            /* Update activity field so contains name and email */
+            vm.myActivities[index].posterName = response.name;
+            vm.myActivities[index].posterEmail = response.email;
+          } else {
+            /* Set name to unknown and display message */
+            activity.poster = 'unknown';
+            if (response) {
+              vm.message = response;
+            } else {
+              vm.message = 'Error getting user information.';
+            }
+          }
+      });
+    }
+
 
     /* Call to get all activities for user */
     activitiesService.getUserActivities(function(result, data) {
@@ -26,26 +47,11 @@ angular.module('clientApp')
         /* Print activity data to screen */
         vm.myActivities = data;
 
-        /* Search through and fill in name data */
+        /* Iterate through each activity in array */
         vm.myActivities.forEach(function(activity, index, actArray) {
 
-          /* We need to make individual requests to get names */
-          profileService.getUserDetails(activity.postedBy,
-            function(nameResult, response) {
-              if (nameResult) {
-                /* Update activity field so contains name and email */
-                actArray[index].posterName = response.name;
-                actArray[index].posterEmail = response.email;
-              } else {
-                /* Set name to unknown and display message */
-                activity.poster = 'unknown';
-                if (response) {
-                  vm.message = response;
-                } else {
-                  vm.message = 'Error getting user information.';
-                }
-              }
-            });
+            /* Fill in names of posters for activities */
+            getNameFromPostedBy(activity, index);
         });
 
       } else {
